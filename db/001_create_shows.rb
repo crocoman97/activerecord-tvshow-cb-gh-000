564537142f89049c8926a569f8b7700db1d30c42
Rauof M <abdelrauof197@gmail.com>
2018-02-12 21:06:14 +0000

    automatically backed up by learn

--- db/001_create_shows.rb
+++ db/001_create_shows.rb
@@ -2,7 +2,9 @@ class CreateShows < ActiveRecord::Migration
   
   def change 
     create_table :shows do |t|
-      
+      t.string :name 
+      t.string :network
+      t.day :day
     end
   end
   
