e7000bbb248bc0a0efe540bbc8e35b388e357902
Rauof M <abdelrauof197@gmail.com>
2018-02-12 21:07:32 +0000

    automatically backed up by learn

--- db/001_create_shows.rb
+++ db/001_create_shows.rb
@@ -4,8 +4,8 @@ class CreateShows < ActiveRecord::Migration
     create_table :shows do |t|
       t.string :name 
       t.string :network
-      t.day :day
-      
+      t.string :day
+      t.integer :rating 
     end
   end
   
